051d718e935ee38eb41a019c2eeb1c7378bdca8a
Micromanagment <Michaelarchwood@gmail.com>
2026-03-12 15:32:35 -0700

    Checks for files and also runs app

--- generate.sh
+++ generate.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 ./vendor/premake/premake5 gmake
-# Build everything
+
+# Create logs directory
+mkdir -p logs
+
+# Makes
 make
+
+# check if file is created
+if [ -f ./bin/Debug-linux-x86_64/App/App ]; then
+
+    # Run main app
+    alacritty -e bash -c "./bin/Debug-linux-x86_64/App/App; exec bash" &
+
+    sleep 0.1
+
+    # Runs logging
+    alacritty -e bash -c "tail -f logs/app.log; exec bash" &
+else
+    echo "Build failed, App not found."
+fi
